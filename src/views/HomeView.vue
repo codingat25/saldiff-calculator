589b0d470653b8618007df77b2920dac1fed78d1
Christian Jay <christianquimporeyes@gmail.com>
2022-06-24 14:02:36 +0800

    setup day1

---
+++ src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <div class="justify-center flex bg-yellow-300 items-center h-screen">
+      <div class="text-4xl">
+        Hello 👋🏼
+      </div>
+   </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'HomeView',
+  components: {
+
+  }
+}
+</script>
